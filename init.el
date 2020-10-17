442d7e2b3b311dc880e35e95c818f8c8ffbeb847
Ellis Kenyo <me@elken.dev>
2020-10-17 12:28:52 +0100

    Autoreload and org-tempo
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- init.el
+++ init.el
@@ -115,7 +115,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       csharp            ; unity, .NET, and mono shenanigans
+       (csharp +lsp)       ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
