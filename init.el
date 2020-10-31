2a7a40100f8148ebc864c670f0a8a87bf72ca018
Ellis Kenyo <me@elken.dev>
2020-10-31 09:26:10 +0000

    Ivy buffs
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- init.el
+++ init.el
@@ -29,7 +29,10 @@
        (company +childframe) ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +childframe
+            +fuzzy
+            +prescient
+            +icons)        ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
