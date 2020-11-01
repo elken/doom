2806632fa32ed73ccf10ae73b20c92070c79fe43
Ellis Kenyo <me@elken.dev>
2020-11-01 16:03:36 +0000

    Add org features
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- init.el
+++ init.el
@@ -154,7 +154,10 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +dragndrop
+            +hugo
+            +present
+            +pretty)       ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
