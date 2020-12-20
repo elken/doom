a20073f054c63f7a5aec5704bd526c26a7f65afc
Ellis Kenyo <me@elken.dev>
2020-12-20 09:54:18 +0000

    Start of exwm setup & rework config reload
    + better config reload
    + exwm startup script
    
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- init.el
+++ init.el
@@ -39,11 +39,11 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;fill-column       ; a `fill-column' indicator
+       ;;fill-column     ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures         ; ligatures and symbols to make your code pretty again
+       (ligatures +extra)  ; ligatures and symbols to make your code pretty again
        ;; minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
