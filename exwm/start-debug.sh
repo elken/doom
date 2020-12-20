9a888d601c637c0710f7e1aa6e2d54fd6c9cb4f6
Ellis Kenyo <me@elken.dev>
2020-12-20 14:12:28 +0000

    Slight refactor of exwm
    = Moved config to own org-file
    + Other org-files to `org-babel-tangle`
    
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- exwm/start-debug.sh
+++ exwm/start-debug.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-exec dbus-launch --exit-with-session emacs -mm -l ~/.doom.d/desktop.el
+exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.doom.d/desktop.el
 # exec picom -b --experimental-backends --dbus --config /home/elken/.config/awesome/configuration/picom.conf
 # exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)
