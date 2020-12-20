935f3feb5ed158c8eaa8af13206a7aa1217410ec
Ellis Kenyo <me@elken.dev>
2020-12-20 17:31:15 +0000

    More progress
    + Ignore everything in exwm dir
    + Make tangled shell scripts executable
    + Transparency
    + DE mode
    + Better `counsel-linux-app`
    
    Signed-off-by: Ellis Kenyo <me@elken.dev>

--- exwm/start-debug.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.doom.d/desktop.el
-# exec picom -b --experimental-backends --dbus --config /home/elken/.config/awesome/configuration/picom.conf
-# exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)
--- init.el
+++ init.el
@@ -55,7 +55,7 @@
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
+       (window-select +numbers)     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
