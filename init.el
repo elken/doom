b6eeca97eab197949a09beaa1dccb38e705e1fec
Ellis Kenyo <me@elken.dev>
2020-12-29 06:31:55 +0000

    Documentation mostly
    + Removed desktop.org
    + Document more in config.org

--- init.el
+++ init.el
@@ -76,8 +76,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons
-              +ranger)             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
