d36822e635e674ec48a004ec9c7a43c9cf94bbca
Ellis Kenyő <me@elken.dev>
2021-02-08 05:42:30 +0000

    Add emacs-everywhere
    + Add emacs-everywhere
    + Add MELPA repo for doom-modeline-now-playing

--- init.el
+++ init.el
@@ -193,6 +193,7 @@
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
+       everywhere
 
        :config
        literate
