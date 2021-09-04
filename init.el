5503cb595751fee98f2463ea027723dacd058837
Ellis Kenyo <ellis@coreblue.co.uk>
2021-09-04 20:33:23 +0100

    fix: Minor improvements
    - Couple of long-time evil annoyances
    - Remove gravatars (they don't work and I don't care enough)
    - Minor which-key changes

--- init.el
+++ init.el
@@ -197,7 +197,7 @@
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       everywhere
+       ;; everywhere
 
        :config
        literate
