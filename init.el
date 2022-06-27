b966c6a0b785ea1a82a8dd09c359403900a0d372
Ellis Kenyő <ellis@coreblue.co.uk>
2022-06-27 09:51:20 +0100

    feat(python): add python
    Python is used a _small_ amount now, so worth adding this in

--- init.el
+++ init.el
@@ -184,7 +184,10 @@
            +tree-sitter)       ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       (python +lsp
+               +pyright
+               +tree-sitter
+               +poetry)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
