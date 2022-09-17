f4500fc266738d93a72ad3745c805c8ff967596d
Ellis Kenyő <ellis@coreblue.co.uk>
2022-09-17 06:38:35 +0100

    fix: eglot o clock

--- init.el
+++ init.el
@@ -113,7 +113,8 @@
        (eval +overlay)       ; run code, run (also, repls)
        gist              ; interacting with github gists
        (lookup +docsets)           ; navigate your code and its documentation
-       (lsp +peek)
+       (lsp +peek
+            +eglot)
        (magit +forge)           ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
