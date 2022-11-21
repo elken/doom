891db9a40bdd5a418b8475ace755408e3221a6e9
Ellis Kenyo <me@elken.dev>
2022-11-21 21:41:40 +0000

    feat(corfu): replace -doc with -popupinfo
    This has now been merged and replaced with a much faster interface

--- modules/completion/corfu/config.el
+++ modules/completion/corfu/config.el
@@ -61,16 +61,6 @@
     (corfu-terminal-mode)))
 
 
-(use-package! corfu-doc
-  :hook (corfu-mode . corfu-doc-mode)
-  :custom
-  (corfu-doc-delay 0)
-  :bind (:map corfu-map
-              ("M-n" . corfu-doc-scroll-down)
-              ("M-p" . corfu-doc-scroll-up)
-              ("M-d" . corfu-doc-toggle)))
-
-
 (use-package! orderless
   :when (modulep! +orderless)
   :init
@@ -150,6 +140,11 @@
   :after corfu
   :bind (:map corfu-map
               ("C-q" . corfu-quick-insert)))
+
+
+(use-package! corfu-popupinfo
+  :after corfu
+  :hook (corfu-mode . corfu-popupinfo-mode))
 
 
 (use-package! evil-collection-corfu
