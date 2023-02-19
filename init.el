96fd14b94ac66045a9353379ef8c00d09c070322
Ellis Kenyo <me@elken.dev>
2023-02-19 15:36:13 +0000

    fix: module updates
    Trim some fat

--- init.el
+++ init.el
@@ -44,16 +44,16 @@
        ;;     +icons)                     ; a search engine for love and life
 
        :ui
-       deft              ; notational velocity for Emacs
+       ;; deft              ; notational velocity for Emacs
        doom                      ; what makes DOOM look the way it does
        doom-dashboard            ; a nifty splash screen for Emacs
        doom-quit                 ; DOOM quit-message prompts when you quit Emacs
-       ;; (emoji +unicode)
+       (emoji +unicode)
        ;; exwm
        ;;fill-column     ; a `fill-column' indicator
        hl-todo            ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides     ; highlighted indent columns
+       ;; indent-guides     ; highlighted indent columns
        ligatures  ; ligatures and symbols to make your code pretty again
        ;; minimap           ; show a map of the code on the side
        modeline    ; snazzy, Atom-inspired modeline, plus API
@@ -63,13 +63,13 @@
        (popup +all
               +defaults) ; tame sudden yet inevitable temporary windows
        ;; tabs              ; a tab bar for Emacs
-       (treemacs +lsp)          ; a project drawer, like neotree but cooler
+       ;; (treemacs +lsp)          ; a project drawer, like neotree but cooler
        ;; unicode           ; extended unicode support for various languages
        (vc-gutter +pretty)         ; vcs diff in the fringe
        ;; vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers)         ; visually switch windows
        workspaces       ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
+       ;; zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere)               ; come to the dark side, we have cookies
@@ -93,7 +93,7 @@
        vc                  ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; the elisp shell that works everywhere
+       ;; eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        vterm                            ; the best terminal emulation in Emacs
