f89697995201a5b367684a019da06bde41b2a8c1
Ellis Kenyo <me@elken.dev>
2024-04-22 14:41:49 +0100

    feat: add ruby setup

--- init.el
+++ init.el
@@ -202,9 +202,11 @@
        ;;raku              ; the artist formerly known as perl6
        rest                             ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;;(ruby +lsp
-       ;;      +tree-sitter
-       ;;      +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +lsp
+             +tree-sitter
+             +rbenv
+             +tree-sitter
+             +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;; (rust +lsp
        ;;       +tree-sitter) ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
